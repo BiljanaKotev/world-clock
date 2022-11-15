056c1c23487bdc58fda4f93af28cee7295f7e74e
biljana_kotev <biljanakotev@gmail.com>
2022-11-15 11:08:06 +0100

    adding js elements for real time and dates

--- js/index.js
+++ js/index.js
@@ -5,5 +5,10 @@
 // let selectDropDown = document.getElementById("#select-element");
 // selectDropDown.eventAddListener("change", selectCity);
 
-let time = document.querySelector(".date");
-time.innerHTML = moment().format("MMM Do YY");
+let dateElement = document.getElementsByClassName("date");
+dateElement[0].innerHTML = moment().format("ddd MMM Do YY");
+dateElement[1].innerHTML = moment().format("ddd MMM Do YY");
+
+let timeElement = document.getElementsByClassName("time");
+timeElement[0].innerHTML = moment().format("hh:mm:ss a");
+timeElement[1].innerHTML = moment().format("hh:mm:ss a");
