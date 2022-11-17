0765491e402b149a44e99284d91fdfafbc927bc0
biljana_kotev <biljanakotev@gmail.com>
2022-11-17 10:24:37 +0100

    added functionality for the dropdown select element

--- js/index.js
+++ js/index.js
@@ -9,24 +9,43 @@
 
 function updateTimeInterval() {
   let dateElement = document.getElementsByClassName("date");
-  dateElement[0].innerHTML = moment.tz("Europe/London").format("ddd MMM Do YY");
-  dateElement[1].innerHTML = moment
+  dateElement[0].innerHTML = moment()
+    .tz("Europe/London")
+    .format("ddd MMM Do YY");
+  dateElement[1].innerHTML = moment()
     .tz("Australia/Sydney")
     .format("ddd MMM Do YY");
 
   let timeElement = document.getElementsByClassName("time");
-  timeElement[0].innerHTML = moment.tz("Europe/London").format("hh:mm:ss");
-  timeElement[1].innerHTML = moment.tz("Australia/Sydney").format("hh:mm:ss");
+  timeElement[0].innerHTML = moment().tz("Europe/London").format("HH:mm:ss");
+  timeElement[1].innerHTML = moment().tz("Australia/Sydney").format("HH:mm:ss");
 
   let timeReference = document.getElementsByClassName("time-reference");
-  timeReference[0].innerHTML = moment().format("a");
-  timeReference[1].innerHTML = moment().format("a");
+  timeReference[0].innerHTML = moment().tz("Europe/London").format("a");
+  timeReference[1].innerHTML = moment().tz("Australia/Sydney").format("a");
 }
 setInterval(updateTimeInterval, 1000);
 
 function dropDownCity(event) {
   let timeZone = event.target.value;
-  let cityTime = moment().tz(timeZone);
+  let cityTimeZone = moment().tz(timeZone);
+  let cityName = timeZone.replace("_", " ").split("/")[1];
+  let timeReference = moment().format("a");
+  let citiesContainer = document.querySelector("#cities-container");
+  citiesContainer.innerHTML = ` <div class="city-container">
+            <div class="city-heading">
+               <h3>${cityName}</h3>
+               <div><date class="date">${cityTimeZone.format(
+                 "ddd MMM Do YY"
+               )}</date></div>
+           </div>
+            <div class="time-container">
+             <time class="time">${cityTimeZone.format("h:mm:ss")}</time>
+               <div class="time-reference-container">
+               <small class="time-reference">${timeReference}</small>
+              </div>
+            </div>
+          </div>`;
 }
 
 let dropDownSelect = document.querySelector("#select-element");
