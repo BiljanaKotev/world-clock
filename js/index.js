1855bfa9369a1ae0aad7b9afb5e58909d1beabcd
biljana_kotev <biljanakotev@gmail.com>
2022-11-17 10:43:20 +0100

    updated dropdown value

--- js/index.js
+++ js/index.js
@@ -1,28 +1,27 @@
-// function dropDownCity(event) {
-//   if (event.target.value.length > 0) {
-
-//   }
-// }
-
-// let selectElement = document.querySelector("#select-element");
-// selectElement.addEventListener("change", dropDownCity);
-
 function updateTimeInterval() {
   let dateElement = document.getElementsByClassName("date");
   dateElement[0].innerHTML = moment()
     .tz("Europe/London")
     .format("ddd MMM Do YY");
-  dateElement[1].innerHTML = moment()
-    .tz("Australia/Sydney")
-    .format("ddd MMM Do YY");
+  if (dateElement[1]) {
+    dateElement[1].innerHTML = moment()
+      .tz("Australia/Sydney")
+      .format("ddd MMM Do YY");
+  }
 
   let timeElement = document.getElementsByClassName("time");
   timeElement[0].innerHTML = moment().tz("Europe/London").format("HH:mm:ss");
-  timeElement[1].innerHTML = moment().tz("Australia/Sydney").format("HH:mm:ss");
+  if (timeElement[1]) {
+    timeElement[1].innerHTML = moment()
+      .tz("Australia/Sydney")
+      .format("HH:mm:ss");
+  }
 
   let timeReference = document.getElementsByClassName("time-reference");
   timeReference[0].innerHTML = moment().tz("Europe/London").format("a");
-  timeReference[1].innerHTML = moment().tz("Australia/Sydney").format("a");
+  if (timeReference[1]) {
+    timeReference[1].innerHTML = moment().tz("Australia/Sydney").format("a");
+  }
 }
 setInterval(updateTimeInterval, 1000);
 
@@ -30,7 +29,7 @@ function dropDownCity(event) {
   let timeZone = event.target.value;
   let cityTimeZone = moment().tz(timeZone);
   let cityName = timeZone.replace("_", " ").split("/")[1];
-  let timeReference = moment().format("a");
+  let timeReference = moment().tz(timeZone).format("a");
   let citiesContainer = document.querySelector("#cities-container");
   citiesContainer.innerHTML = ` <div class="city-container">
             <div class="city-heading">
